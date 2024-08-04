3eadeca85123c4a7ce8899dd2493720527568b93
elmurat-karabekov <karabekov.elmurat@yandex.kz>
2024-08-04 11:01:35 +0500

    feat/ Api class implemented

---
+++ src/components/LarekApi.ts
@@ -0,0 +1,30 @@
+import {
+	ILarekApi,
+	IProduct,
+	ApiListResponse,
+	IOrder,
+	IOrderResult,
+	IApi,
+} from '../types';
+
+export class LarekApi implements ILarekApi {
+	private baseApi: IApi;
+	readonly cdn: string;
+
+	constructor(baseApi: IApi, cdn: string) {
+		this.baseApi = baseApi;
+		this.cdn = cdn;
+	}
+
+	async getProducts(): Promise<IProduct[]> {
+		const data = await this.baseApi.get<ApiListResponse<IProduct>>('/product');
+		return data.items.map((product) => ({
+			...product,
+			image: this.cdn + product.image,
+		}));
+	}
+
+	async orderProducts(order: IOrder): Promise<IOrderResult> {
+		return await this.baseApi.post<IOrderResult>('/order', order);
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { Api } from './components/base/api';
+import { LarekApi } from './components/LarekApi';
 import './scss/styles.scss';
+import { API_URL, CDN_URL } from './utils/constants';
+
+const baseApi = new Api(API_URL);
+const larekApi = new LarekApi(baseApi, CDN_URL);
+
+console.log(larekApi.getProducts().then((data) => console.log(data)));
